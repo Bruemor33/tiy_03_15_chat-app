55c6488108cb442b45698fc5a32f5e7c3537b8c1
Bruemor <arymer1@gmail.com>
2016-03-16 14:54:21 -0400

    Added a state

--- app/scripts/components/chat-comp.js
+++ app/scripts/components/chat-comp.js
@@ -13,29 +13,6 @@ var application = require('../model/app-model');
 
 var chatRoom = new application.ChatCollection();
 
-var ChatMessages = React.createClass({
-  mixins: [Backbone.React.Component.mixin],
-
-  //Setting up some properties for main Chat Component
-  //We want the properties to be passed into the "parent" ChatComponent
-  render: function(){
-    return (
-      <form data-validate="parsley" onSubmit={this.props.handleSubmit} id="message-form" className="input-group">
-        <input
-          parsley-type="text"
-          className="form-control"
-          id="message-input"
-          data-required="true"
-          data-parsley-minlength="1"
-          value="this.props.userMessage"
-          onChange="this.props.handleChange"
-        />
-        <button className="btn btn-primary" id="chat-sumbit" disabled="true" role="button">Enter</button>
-      </form>
-    )
-  }
-});
-
 var ChatComponent = React.createClass({
   mixins: [Backbone.React.Component.mixin],
 
